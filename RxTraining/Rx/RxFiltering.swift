96ad30636ada313c58051edc25323c22af8d9bc6
KorobskoyRoman <shopone023@gmail.com>
2022-08-24 00:15:02 +0300

    Filtering

--- RxTraining/Rx/RxFiltering.swift
+++ RxTraining/Rx/RxFiltering.swift
@@ -18,12 +18,14 @@ class RxFiltering {
      - parameter source: Исходная последовательность
      - parameter condition:  Условие, которому должен удовлетворять элемент
      - returns: Результирующая последовательность
-    */
+     */
     func firstValueWithCondition<T>(
         source: Observable<T>,
         condition: @escaping (T) -> Bool
     ) -> Observable<T> {
-       return .error(NotImplemetedError())
+        source
+            .filter { condition($0) }
+            .take(1)
     }
     
     /**
@@ -33,13 +35,15 @@ class RxFiltering {
      - parameter pickFirstValuesCount: Кол-во элементов, которые должны попасть в последовательность
      - parameter notPickFirstValuesCount: Кол-во элементов, которые должны быть пропущены
      - returns: Результирующая последовательность
-    */
+     */
     func pickFirstValues<T>(
         source: Observable<T>,
         pickFirstValuesCount: Int,
         notPickFirstValuesCount: Int
     ) -> Observable<T> {
-        return .error(NotImplemetedError())
+        source
+            .skip(notPickFirstValuesCount)
+            .take(pickFirstValuesCount)
     }
     
     /**
@@ -49,13 +53,15 @@ class RxFiltering {
      - parameter scheduler:  Scheduler, на котором должно производиться ожидание элементов
      - parameter interval: Время, в течение которого происходит ожидание элементов
      - returns: Результирующая последовательность
-    */
+     */
     func takeLastWithDuration<T>(
         source: Observable<T>,
         scheduler: SchedulerType,
         interval: DispatchTimeInterval
     ) -> Observable<T> {
-        return .error(NotImplemetedError())
+        source
+            .take(interval, scheduler: scheduler)
+            .takeLast(1)
     }
     
     /**
@@ -65,13 +71,14 @@ class RxFiltering {
      - parameter waitingTime: Время ожидания следующего элемента
      - parameter scheduler: Scheduler, на котором должно производиться ожидание элементов
      - returns: Результирующая последовательность
-    */
+     */
     func ignoreDublicatesWithWaitingError<T: Comparable>(
         source: Observable<T>,
         waitingTime: DispatchTimeInterval,
         scheduler: SchedulerType
     ) -> Observable<T> {
-        return .error(NotImplemetedError())
+        source.distinctUntilChanged()
+            .timeout(waitingTime, scheduler: scheduler)
     }
     
     /**
@@ -85,7 +92,7 @@ class RxFiltering {
      - attention: Для реализации данной последовательности необходимо использовать методы трансформации
      (рекомендуется решить задания из RxTransforming)
      - returns: Результирующая последовательность
-    */
+     */
     func searchWhenFinishedTyping(
         searchStringObservable: Observable<String>,
         waitingTime: DispatchTimeInterval,
@@ -94,18 +101,24 @@ class RxFiltering {
         // Каталог, в котором необходимо осуществлять поиск строк
         let searchCatalog = ["unicorns", "popcorn", "corn", "porridge", "pork", "portal"]
         
-        return Observable<[String]>.just(searchCatalog)
-            .concat(Observable<[String]>.error(NotImplemetedError()))
+                return Observable<[String]>.just(searchCatalog)
+                    .concat(Observable<[String]>.error(NotImplemetedError()))
+//        let source = Observable.just(searchCatalog)
+//            .map {
+//                $0.filter{ $0.hasPrefix(searchStringObservable)}
+//            }
+//        return source
     }
-    
     /**
      Каждый раз, когда switcher испускает false, а затем true (или если true первый элемент),
      то в результирующую последовательность попадает последний элемент из source (если он уже не попал туда ранее):
      - parameter source: Исходная последовательность
      - parameter switcher: Последовательность, по событиям которой происходит дублирование элементов из source
      - returns: Результирующая последовательность
-    */
+     */
     func releaseElementWhenSwitched<T>(source: Observable<T>, switcher: Observable<Bool>) -> Observable<T> {
         return .error(NotImplemetedError())
+//        source
+//            .takeLast(1)
     }
 }
